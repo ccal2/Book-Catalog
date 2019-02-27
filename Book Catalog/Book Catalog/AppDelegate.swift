3bd1c30de1ff9a9318d64ba0beee573475104210
Unknown <ccal2@cin.ufpe.br>
2019-02-27 11:07:48 -0300

    Create Xcode project

---
+++ Book Catalog/Book Catalog/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Book Catalog
+//
+//  Created by Carolina Lopes on 27/02/19.
+//  Copyright © 2019 Carolina Lopes. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    // MARK: - Properties
+
+    var window: UIWindow?
+
+
+    // MARK: - Methods
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
